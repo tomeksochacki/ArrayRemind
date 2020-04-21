2d7e03331ea251fa06555df0e6b2e7abfbb05600
tomek <tomeksochacki@wp.pl>
2020-04-21 10:02:55 +0200

    Origin master

---
+++ src/ArrayRemind.java
@@ -0,0 +1,13 @@
+public class ArrayRemind {
+    public static void main(String[] args) {
+        String[]firstName=new String[4];
+
+        firstName[0]="Tom";
+        firstName[1]="Tomek";
+        firstName[2]="Tomasz";
+        firstName[3]="Tomuś";
+
+        int[]number=new int[]{1,2,3,4,5};
+        
+    }
+}
